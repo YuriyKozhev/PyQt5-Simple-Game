4c92278beb884a4db19e8f3629047e3b5e829900
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-03-28 16:49:04 +0300

    update main file to create Scene object

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from scene import Scene
+
+from PyQt5.QtWidgets import QApplication
+
+import sys
+
+
 if __name__ == "__main__":
-    print("main")
+    app = QApplication(sys.argv)
+    scene = Scene()
+    sys.exit(app.exec_())
